9dc83adb86139f2ed13980720ade8226523d7189
MrCelticFox <diarmaid@mrcelticfox.com>
2015-12-10 21:09:53 +0000

    cleaned up Main class

--- Main.java
+++ Main.java
@@ -1,59 +1,27 @@
-import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 
 
-
-
-public class Main
-{
-
+public class Main {
     /**
-     * Cell size in pixels.
+     * Creates the UI, all components are defined in the natural reading order
+     * (left to right, top to bottom) unless otherwise specified.
      */
-    public static final int SIZE = 32; 
-
-    /**
-     * Number of unique icons.
-     */
-    public static final int TYPES = 6;
-
-    /**
-     * Game mode: 0 for Mine, 1 for Farm
-     */
-    public static final int MODE = 0;
-
-    /**
-     * Font size for displaying score.
-     */
-    public static final int SCORE_FONT_SIZE = 20; 
-
-
-    //-------------------------------------------------------------------------------------------
-
-
-    /**
-     * Helper method for instantiating the components.  This
-     * method should be executed in the context of the Swing
-     * event thread only.
-     */
-    private static void create()
-    {
+    private static void create() {
 	JFrame frame;
 	frame = new JFrame();
 	frame.setBounds(100, 100, 450, 300);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
 	GridBagLayout gridBagLayout = new GridBagLayout();
 	gridBagLayout.columnWidths = new int[]{0, 0, 0};
 	gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
@@ -62,7 +30,6 @@ public class Main
 	frame.getContentPane().setLayout(gridBagLayout);
 
 	JLabel title = new JLabel("Transport Portal Route Planner");
-	//lblTransport.setText(text);
 	GridBagConstraints gbc_title = new GridBagConstraints();
 	gbc_title.insets = new Insets(5, 5, 30, 5);
 	gbc_title.gridwidth = 3;
@@ -85,7 +52,7 @@ public class Main
 	frame.getContentPane().add(opt2_lbl, gbc_opt2_lbl);
 
 	String locations[] = {"one", "two", "3"}; //define elsewhere
-	final JComboBox start = new JComboBox(locations);
+	final JComboBox<String> start = new JComboBox<String>(locations);
 	GridBagConstraints gbc_start = new GridBagConstraints();
 	gbc_start.insets = new Insets(10, 10, 10, 10);
 	gbc_start.fill = GridBagConstraints.HORIZONTAL;
@@ -100,7 +67,7 @@ public class Main
 	gbc_lbl.gridy = 2;
 	frame.getContentPane().add(lbl, gbc_lbl);
 
-	final JComboBox destination = new JComboBox(locations);
+	final JComboBox<String> destination = new JComboBox<String>(locations);
 	GridBagConstraints gbc_destination = new GridBagConstraints();
 	gbc_destination.insets = new Insets(10, 10, 10, 10);
 	gbc_destination.fill = GridBagConstraints.HORIZONTAL;
@@ -116,7 +83,7 @@ public class Main
 	frame.getContentPane().add(type_lbl, gbc_type_lbl);
 
 	String options[] = {"cheapest", "quickest"}; 
-	JComboBox type = new JComboBox(options);
+	JComboBox<String> type = new JComboBox<String>(options);
 	GridBagConstraints gbc_type = new GridBagConstraints();
 	gbc_type.insets = new Insets(10, 10, 10, 10);
 	gbc_type.fill = GridBagConstraints.HORIZONTAL;
@@ -124,7 +91,8 @@ public class Main
 	gbc_type.gridy = 3;
 	frame.getContentPane().add(type, gbc_type);
 
-	final JLabel results_lbl = new JLabel(""); //text set later
+	// label for the results, text will be set when the user presses the submit button
+	final JLabel results_lbl = new JLabel("");  
 	GridBagConstraints gbc_results_lbl = new GridBagConstraints();
 	gbc_results_lbl.insets = new Insets(10, 10, 10, 10);
 	gbc_results_lbl.gridwidth = 3;
@@ -140,6 +108,7 @@ public class Main
 		path = path + start.getItemAt(start.getSelectedIndex()) + "\n";
 		path = path + destination.getItemAt(destination.getSelectedIndex()) + "\n";
 		results_lbl.setText(path);
+		// TODO: replace contents with pathGenerator method
 	    }
 	});
 	GridBagConstraints gbc_submit = new GridBagConstraints();
@@ -148,31 +117,28 @@ public class Main
 	gbc_submit.gridy = 3;
 	frame.getContentPane().add(submit, gbc_submit);
 
-	// rock and roll...
+	// show the whole thing
 	frame.setVisible(true);
     }
 
 
-    //-------------------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------
 
 
     /**
-     * Entry point.  Main thread passed control immediately
-     * to the Swing event thread.
+     * Starts the program. The list of transporters is loaded and then the Main 
+     * thread passes control immediately to the Swing event thread.
+     * 
      * @param args not used
      */
-    public static void main(String[] args)
-    {
-	Runnable r = new Runnable()
-	{
-	    public void run()
-	    {
+    public static void main(String[] args) {
+	// TODO: implement transporter loading
+	
+	EventQueue.invokeLater(new Runnable() {
+	    public void run() {
 		create();
 	    }
-	};
-	SwingUtilities.invokeLater(r);
+	});
     }
 
 }
-
-
